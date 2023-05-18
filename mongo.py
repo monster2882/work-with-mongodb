d492479238d37c0e0779415fa08bbadac7e9997f
monster2882 <monster2882@mail.ru>
2023-05-18 11:29:22 +0300

    Add new function(scope_auto)
    Данная функция вывод данные по фио

--- mongo.py
+++ mongo.py
@@ -37,4 +37,59 @@ def check_work():
         print(f'Сумма: {profit}')
         print("==============================")
 
+
+def scope_auto():
+    autos_collections = db['autos']
+    trips_collections = db['trips']
+    clients_collections = db['clients']
+
+    clients = clients_collections.find()
+
+    numbers = []
+
+    for client in clients:
+        client_id = client['client_id']
+        name = client['name']
+        surname = client['surname']
+        father_name = client['otchestvo']
+
+        profit = 0.0
+        distance = 0.0
+
+        trips = trips_collections.find({'client_id': client_id})
+
+        for trip in trips:
+            auto_id = trip['auto_id']
+            empty_count = trip['kol_pust']
+            full_count = trip['kol_gruzh']
+            weight = trip['weight']
+            date = trip['date']
+
+            autos = autos_collections.find({'auto_id': auto_id})
+            count_trips = trips_collections.count_documents({'auto_id': auto_id})
+
+            for auto in autos:
+                auto_id = auto['auto_id']
+                number = auto['nomber']
+                empty_price = auto['st_pust']
+                full_price = auto['st_grug']
+                profit = empty_price * empty_count + full_count * full_price
+                distance = weight * (empty_count + full_count)
+
+
+        print(f'Имя: {name}')
+        print(f'Фамилия: {surname}')
+        print(f'Отчество: {father_name}')
+        print(f'Кол-во заказов {count_trips}')
+        print(f'Тонно-киллометры: {distance}')
+        print(f'Сумма: {profit}')
+        print("==============================")
+
+
+print('Check_work function')
+print()
 check_work()
+print("Scope_auto function")
+print()
+scope_auto()
+
