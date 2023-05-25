204e8745818b4dc29cf077a6af335bfb523e2e2f
monster2882 <monster2882@mail.ru>
2023-05-25 11:07:22 +0300

    Написаны все функции
    Функции выводят группировку по различным критериям

--- mongo.py
+++ mongo.py
@@ -28,24 +28,22 @@ def check_work():
             empty_count = trip['kol_pust']
             full_count = trip['kol_gruzh']
             weight = trip['weight']
-            profit = empty_price * empty_count + full_count * full_price
-            distance = weight * (empty_count + full_count)
+            profit += empty_price * empty_count + full_count * full_price
+            distance += weight * (empty_count + full_count)
 
         print(f'Гос.номер: {number}')
-        print(f'Кол-во заказов {count_trips}')
+        print(f'Кол-во заказов: {count_trips}')
         print(f'Тонно-киллометры: {distance}')
         print(f'Сумма: {profit}')
         print("==============================")
 
-
+#Функция выводит группировку по фио
 def scope_auto():
     autos_collections = db['autos']
     trips_collections = db['trips']
     clients_collections = db['clients']
 
     clients = clients_collections.find()
-
-    numbers = []
 
     for client in clients:
         client_id = client['client_id']
@@ -57,7 +55,7 @@ def scope_auto():
         distance = 0.0
 
         trips = trips_collections.find({'client_id': client_id})
-
+        number = set()
         for trip in trips:
             auto_id = trip['auto_id']
             empty_count = trip['kol_pust']
@@ -66,24 +64,79 @@ def scope_auto():
             date = trip['date']
 
             autos = autos_collections.find({'auto_id': auto_id})
-            count_trips = trips_collections.count_documents({'auto_id': auto_id})
-
+            count_trips = trips_collections.count_documents({'client_id': client_id})
             for auto in autos:
                 auto_id = auto['auto_id']
-                number = auto['nomber']
+                number.add(auto['nomber'])
                 empty_price = auto['st_pust']
                 full_price = auto['st_grug']
-                profit = empty_price * empty_count + full_count * full_price
-                distance = weight * (empty_count + full_count)
-
+                profit += empty_price * empty_count + full_count * full_price
+                distance += weight * (empty_count + full_count)
 
         print(f'Имя: {name}')
         print(f'Фамилия: {surname}')
         print(f'Отчество: {father_name}')
         print(f'Кол-во заказов {count_trips}')
         print(f'Тонно-киллометры: {distance}')
+        print(f'Номера: {[i for i in number]}')
         print(f'Сумма: {profit}')
         print("==============================")
+
+#Функция выводит группировку по моделям
+def scope_of_work():
+    autos_collections = db['autos']
+    trips_collections = db['trips']
+    clients_collections = db['clients']
+
+    autos = autos_collections.find()
+    model_totals = {}  # Словарь для хранения сумм по моделям
+
+    for auto in autos:
+        auto_id = auto['auto_id']
+        model = auto['model']
+        empty_price = auto['st_pust']
+        full_price = auto['st_grug']
+        number = auto['nomber']
+
+        profit = 0.0
+        distance = 0.0
+        count_trips = 0
+
+        auto_data = autos_collections.find_one({'model': model})
+        trips = trips_collections.find({'auto_id': auto_id})
+        for trip in trips:
+            client_id = trip['client_id']
+            count_trips = trips_collections.count_documents({'auto_id': auto_id})
+            empty_count = trip['kol_pust']
+            full_count = trip['kol_gruzh']
+            weight = trip['weight']
+            profit += empty_price * empty_count + full_count * full_price
+            distance += weight * (empty_count + full_count)
+
+        # Добавляем значения в словарь сумм по моделям
+        if model in model_totals:
+            model_totals[model]['count_trips'] += count_trips
+            model_totals[model]['distance'] += distance
+            model_totals[model]['profit'] += profit
+        else:
+            model_totals[model] = {
+                'count_trips': count_trips,
+                'distance': distance,
+                'profit': profit
+            }
+
+    # Выводим результаты суммирования по моделям
+    for model, totals in model_totals.items():
+        count_trips = totals['count_trips']
+        distance = totals['distance']
+        profit = totals['profit']
+
+        print(f'Модель: {model}')
+        print(f'Кол-во заказов: {count_trips}')
+        print(f'Тонно-киллометры: {distance}')
+        print(f'Сумма: {profit}')
+        print("==============================")
+
 
 
 print('Check_work function')
@@ -92,4 +145,7 @@ check_work()
 print("Scope_auto function")
 print()
 scope_auto()
+print("Scope_of_work")
+print()
+scope_of_work()
 
